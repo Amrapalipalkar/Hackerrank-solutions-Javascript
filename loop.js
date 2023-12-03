1f3a4ee1e37b520e502c628b28412b32b9822243
Er. Amrapali Palkar <80786215+Amrapalipalkar@users.noreply.github.com>
2023-12-03 13:07:01 +0530

    loop.js

--- loop.js
+++ loop.js
@@ -1,5 +1,5 @@
 function vowelsAndConsonants(s) {
-    var vowels = ["a", "e", "i", "o", "u"];
+    var vowels = ["a", "e", "i", "o", "u"]
     for (var i = 0; i < s.length; i++){
         if (vowels.indexOf(s[i]) > -1){
             console.log(s[i]);
